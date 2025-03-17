c172903f5b78407961a88efa822a10e7bbf63666
Bernie <ptyc4076@gmail.com>
2025-03-17 14:26:28 +0800

    Changed file name to Python format. 03/17

--- for-demo-purposes/fastapi_demo.py
+++ for-demo-purposes/fastapi_demo.py
@@ -15,4 +15,4 @@ async def root(response: Response):
 
 if __name__ == "__main__":
     # host="0.0.0.0" 使伺服器可從外部訪問，port 可自行調整
-    uvicorn.run(app='app', host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app='fastapi_demo:app', host="0.0.0.0", port=8000, reload=True)
